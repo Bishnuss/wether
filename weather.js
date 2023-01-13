a2a5d7e44e7ad37ed65c213d1bd0add8c0a48bae
Bishnus HP <bishnuvikraman4@gmail.com>
2023-01-13 20:59:40 +0530

    change

--- weather.js
+++ weather.js
@@ -1,21 +1,21 @@
-function search(){
-    city=cname.value
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5b4bee0ba241d092159faf007e166080`).then(d=>d.json())
-    .then(res=>displaydata(res))
+function search() {
+  city = cname.value
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5b4bee0ba241d092159faf007e166080`).then(d => d.json())
+    .then(res => displaydata(res))
 
 }
-function displaydata(citydetails){
-        cname=citydetails.name
-        ctemp=citydetails.main.temp 
-       humidity=citydetails.main.humidity
-          speed=citydetails.wind.speed
-        rise=citydetails.sys.sunrise
-       set=citydetails.sys.sunset
-           climate=citydetails.weather[0].main
-          method=Math.floor(ctemp- 273)
+function displaydata(citydetails) {
+  cname = citydetails.name
+  ctemp = citydetails.main.temp
+  humidity = citydetails.main.humidity
+  speed = citydetails.wind.speed
+  rise = citydetails.sys.sunrise
+  set = citydetails.sys.sunset
+  climate = citydetails.weather[0].main
+  method = Math.floor(ctemp - 273)
 
 
-htmldata = `<section class="vh-100" >
+  htmldata = `<section class="vh-100" >
 <div class="container  h-100">
 
   <div class="row justify-content-center align-items-center h-100">
@@ -26,15 +26,15 @@ htmldata = `<section class="vh-100" >
 
           <div class="bg-secondary rounded-pill">
           <h4 class="text-warning">City</h4>
-            <h3 class="text-white text-center fw-bold">${cname}</h3>
+            <h3 class="text-white text-center fw-bold mb-4">${cname}</h3>
           </div>
 
-          <div class="bg-secondary rounded-pill">
+          <div class="bg-secondary rounded-pill mb-2">
           <h4 class="text-warning">Temperature</h4>
           <h3 class="text-white text-center fw-bold">${method}&#176;C</h3>
           </div>
 
-          <div class="bg-secondary rounded-pill">
+          <div class="bg-secondary rounded-pill mb-2">
           <h4 class="text-warning">Humidity</h4>
             <h3 class="text-white text-center fw-bold">${humidity} %</h3>
           </div>
@@ -58,16 +58,6 @@ htmldata = `<section class="vh-100" >
           <h4 class="text-warning">Climate</h4>
             <h3 class="text-white text-center fw-bold">${climate}</h3>
           </div>
-
-
-
-
-
-
-
-
-
-
           </div>
         </div>
       </div>
@@ -76,6 +66,6 @@ htmldata = `<section class="vh-100" >
 
 </div>
 </section>`
-    result.innerHTML= htmldata
+  result.innerHTML = htmldata
 
 }
